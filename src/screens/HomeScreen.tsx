851e1848f0c32cba56c52478314006828818d511
fhd-codes <fhd1103@outlook.com>
2024-12-01 14:15:45 +0500

    added screen files

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import { Devvit } from '@devvit/public-api';
+
+const HomeScreen = () => {
+    return (
+        <vstack height="100%" width="100%" alignment="middle center" backgroundColor="#f0f0f0" padding="medium">
+            Home Screen
+        </vstack>
+    );
+};
+
+export default HomeScreen;
