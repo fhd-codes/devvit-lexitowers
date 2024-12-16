4b04d841d263fca479b1bbae2e7ca9727da0e4e3
fhd-codes <fhd1103@outlook.com>
2024-12-16 23:42:05 +0500

    how to play screen design updated

--- src/screens/HowToPlayScreen.tsx
+++ src/screens/HowToPlayScreen.tsx
@@ -1,9 +1,36 @@
 import { Devvit } from '@devvit/public-api';
+import { PageProps } from "../interfaces.ts";
 
-const HowToPlayScreen = () => {
+import { GAME_CONTAINER } from "../global-constants.ts";
+
+const HowToPlayScreen = ({setPage}: PageProps) => {
     return (
-        <vstack height="100%" width="100%" alignment="middle center" backgroundColor="#f0f0f0" padding="medium">
-            HowToPlay Screen
+        <vstack height="100%" width="100%" alignment="middle center" padding="medium">
+            <vstack height="100%" width={GAME_CONTAINER} alignment="middle center" gap='small'>
+                {/* ------ Top buttons container ------ */}
+                <hstack width='100%' height='8%'>
+                    <button icon='home' size='small' onPress={()=>setPage('home')}>Home</button>
+                </hstack>
+
+                {/* ------ Steps info section ------ */}
+                <hstack width='100%' height='82%'>
+                    <vstack width='100%' backgroundColor='white' border='thin' borderColor='green' cornerRadius='medium'>
+                        <text>Steps will be shown here.</text>
+                    </vstack>
+                </hstack>
+
+                {/* ------ Steps navigation buttons container ------ */}
+                <hstack width='100%' height='10%'>
+                    <hstack width='50%' height='100%' alignment='start middle'>
+                        <button icon='left' appearance='success' />
+                    </hstack>
+
+                    <hstack width='50%' height='100%' alignment='end middle'>
+                        <button icon='right' appearance='success' />
+                    </hstack>
+                </hstack>
+                {/* ------------- */}
+            </vstack>
         </vstack>
     );
 };
